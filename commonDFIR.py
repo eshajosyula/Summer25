054f8647d4d65a5cdc48b60f3118eb40f30c258d
eshajosyula <gayatri.josyula@gmail.com>
2025-06-11 11:07:57 -0400

    some more relevant basic dfir commands

--- commonDFIR.py
+++ commonDFIR.py
@@ -1,4 +1,4 @@
-import os, time
+import os, time, platform
 
 # list all files w/ last modified time
 def recentMods():
@@ -17,6 +17,7 @@ def printEnvVars():
     for key in os.environ:
         print(key, "=", os.environ[key])
 
+# print names of suspicious files
 def susFiles():
     print("\n")
     print("============ sus files =============")
@@ -25,10 +26,41 @@ def susFiles():
         if any(file.endswith(ext) for ext in sus):
             print("SUS : ", file)
 
+# print all open ports
+def getOpenPorts():
+    print("checking 4 open network connections...")
+    connections = os.popen("netstat -an").read()
+
+    for line in connections.splitlines():
+        if "ESTABLISHED" in line or "LISTEN" in line:
+            print("->", line)
+
+# print all running processes
+def openProc():
+    print("\n======================================================================================")
+    print("running processes :\n")
+    processes = os.popen("ps aux").read()
+    print(processes)
+
+# prints out known malware in computer files
+def checkForMalware():
+    iocFiles = [".Xauthority", "id_rsa", "malware.sh"]
+    paths2check = ["/tmp", "/home", "/var/tmp"]
+
+    for path in paths2check:
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                if file in iocFiles:
+                    print("found sus file : ", os.path.join(root, file))
+
+
 def main():
-    recentMods()
-    printEnvVars()
-    susFiles()
+    # recentMods()
+    # printEnvVars()
+    # susFiles()
+    getOpenPorts()
+    # openProc()
+    checkForMalware()
 
 
 if __name__ == "__main__":
